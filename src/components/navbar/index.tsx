f97ee1d568dc2035da62da93545492647474bc6f
Daniel Ugbeye <ugbeyellionz@gmail.com>
2024-02-20 13:45:40 +0100

    fix type errors

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -15,7 +15,6 @@ import { FundnetLogo } from "../logo";
 import { Button } from "../ui/button";
 import { ScrollArea } from "../ui/scroll-area";
 import { Sheet, SheetContent } from "../ui/sheet";
-import { UserNavbarDropdown } from "../user-navbar-dropdown";
 
 export interface NavbarProps
   extends Omit<HTMLAttributes<HTMLElement>, "children"> {
